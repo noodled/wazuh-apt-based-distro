e3055abafa74537b779631c694922b6e147abea2
noodled <3495329+noodled@users.noreply.github.com>
2022-03-10 09:21:26 +0100

    Add files via upload

---
+++ inst-fullwazuh.sh
@@ -0,0 +1,7 @@
+withing 3 min WAZUH Suricata owlhzeek will install
+This can be very timeconsuming for the compile of Zeek
+sleep 180
+wazuh-unattended-APT-installation.sh
+owlhsuricata508.sh
+owlhzeek420.sh
+set-ufw-4-elk.sh
